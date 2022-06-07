b984e25dfbb2e5dd361f6c4c46b4c245737cdcd2
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-06-07 19:19:37 -0300

    Feito a 2, porém ao voltar ele não filtra o retorno. Ex. 'o', depois 'oo'. Ao voltar para 'o', ele não esta filtrando novamente a api do zero

--- src/context/StarProvider.js
+++ src/context/StarProvider.js
@@ -1,21 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import response from '../testData';
 // import PropTypes from 'prop-types';
 import Context from './Context';
 
 function StarProvider({ children }) {
   const [star, setStar] = useState([]);
+  const [name, setName] = useState({
+    filterByName: {
+      name: '',
+    },
+  });
+
   const apiStar = async () => {
     try {
-      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-      const data = await response.json();
-      setStar(data.results);
+      // const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      // const data = await response.json();
+      setStar(response.results);
     } catch (e) {
       console.log(e);
     }
   };
+  useEffect(() => {
+    apiStar();
+  }, []);
+
+  useEffect(() => {
+    if (name.filterByName.name.length !== 0) {
+      apiStar();
+      setStar(star.filter((item) => item.name.includes(name.filterByName.name)));
+    } else { apiStar(); }
+
+    // console.log(teste);
+  }, [name]);
+
+  function handleChange({ target: { value } }) {
+    setName({
+      filterByName: {
+        name: value,
+      },
+    });
+  }
+
   const contextType = {
-    apiStar,
     star,
+    handleChange,
   };
   return (
     <Context.Provider value={ contextType }>
